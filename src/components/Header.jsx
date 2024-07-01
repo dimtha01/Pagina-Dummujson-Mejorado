ccc975885ab6d9fc52076b80bfc4a541db05cc54
dimtha01 <richarsonmartinez400@gmail.com>
2024-07-01 13:43:09 -0400

    Version de Prueba

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
     };
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-black" data-bs-theme="dark">
+            <nav className="navbar navbar-expand-lg bg-header" data-bs-theme="dark">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">Navbar</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
